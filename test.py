85160f8678669c487577d2eaad98acac3017e022
wnsgks3153 <72612538+wnsgks3153@users.noreply.github.com>
2020-10-10 15:19:56 +0900

    Update test.py
    findMax함수 내 변수명 변경(biggest -> big)

--- test.py
+++ test.py
@@ -1,18 +1,18 @@
 def findMax(a, b, c):
     if a > b:
-        biggest = a
+        big = a
     else:
-        biggest = b
+        big = b
 
-    if c > biggest:
-        biggest = c
+    if c > big:
+        big = c
 
-    return biggest
+    return big
 
 a = int(input("첫 번째 숫자 입력: "))
 b = int(input("두 번째 숫자 입력: "))
 c = int(input("세 번째 숫자 입력: "))
 
-biggest = findMax(a, b, c)
+big = findMax(a, b, c)
 
-print(a, b, c, "중 가장 큰 수는 ",biggest, "입니다.")
+print(a, b, c, "중 가장 큰 수는 ",big, "입니다.")
